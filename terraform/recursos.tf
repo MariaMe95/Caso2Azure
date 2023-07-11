a73f0312f7e894048ecba0ece37b03d5dd2d8ab3
root <root@UbuntuDevops.myguest.virtualbox.org>
2023-07-11 09:37:10 +0200

    añadir aks

--- terraform/recursos.tf
+++ terraform/recursos.tf
@@ -2,6 +2,28 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "myaks1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "myaks-1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = var.network_name
   address_space       = ["10.0.0.0/16"]
